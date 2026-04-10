dc8e2285ebc7ce42a704db2358859950dbd2a987
Taghreed Ghassan Oyoun <taghreedoyoun@gmail.com>
2026-04-10 19:41:23 +0200

    Update main.py after adding thompson file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from parser import preprocess_regex
 from postfix import regex_to_postfix
+from thompson import postfix_to_nfa, print_nfa
 
 if __name__ == "__main__":
     result = preprocess_regex()
     
     if result:
         print("Processed Regex Ready for Next Step:", result)
-
         postfix = regex_to_postfix(result)
         print("Postfix Ready for Next Step:", postfix)
+        
+        nfa = postfix_to_nfa(postfix)
+        print_nfa(nfa)
